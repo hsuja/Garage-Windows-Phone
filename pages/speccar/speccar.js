058b6b335ea171ab11950d66324b3bf23eb8f674
hsuja <hsuja@onid.oregonstate.edu>
2015-09-02 15:15:05 -0700

    Update speccar.js

--- pages/speccar/speccar.js
+++ pages/speccar/speccar.js
@@ -14,24 +14,19 @@
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         
-        
         ready: function (element, options) {
             // TODO: Initialize the page here.
 
             document.getElementById("loc_status").innerHTML = "Finding location...";
             getLoc();
             var tempthis = this;
-
-            
-            
-
             var client = new Windows.Web.Http.HttpClient();
- 
             var uri = "http://fleetmgr2212.appspot.com/user/" + WinJS.Application.sessionState.uid + "/specCar/" + WinJS.Application.sessionState.sid;
 
             if (WinJS.Application.sessionState.uid == null) {
                 WinJS.Navigation.navigate('/pages/login/login.html');
             } else {
+                
                 client.getAsync(new Windows.Foundation.Uri(uri)).done(function (result) {
 
                     var jsonResult = JSON.parse(result.content.toString());
@@ -44,24 +39,20 @@
                 });
             }
 
-
-            
             uri += "/fillUp";
             if (WinJS.Application.sessionState.uid == null) {
                 WinJS.Navigation.navigate('/pages/login/login.html');
             } else {
+                
                 client.getAsync(new Windows.Foundation.Uri(uri)).done(function (result) {
 
                     var jsonResult = JSON.parse(result.content.toString());
-
-
 
                     if (jsonResult.fillUps.length == 0) {
                         document.getElementById("fillupList").innerHTML = "No fill ups in database.";
                     }
 
                     for (var i in jsonResult.fillUps) {
-
 
                         var out = 'Date: ' + jsonResult.fillUps[i].date.toString() + '<br>Distance: ' + jsonResult.fillUps[i].distance.toString() +
                                    '<br>Fuel Used: ' + jsonResult.fillUps[i].fuelused.toString() + '<br>MPG: ' + jsonResult.fillUps[i].fuelecon.toString();
@@ -71,16 +62,11 @@
                         }
 
                         out += '<br><button class="delete" id="' + jsonResult.fillUps[i].key.toString() + '">Delete</button><br><br>';
-
                         document.getElementById("fillupList").innerHTML += out;
-
-
 
                     }
 
-
                     //bind event handlers
-
                     WinJS.Utilities.query("button.delete").listen("click", tempthis.deleteClickHandler, false);
 
                     var addFillUpButton = document.getElementById("addFillUp");
@@ -95,37 +81,29 @@
                         WinJS.Application.sessionState.sid = null;
                         WinJS.Navigation.navigate('/pages/login/login.html');
                     }, false);
-
                 });
             }
-
         },
 
         deleteClickHandler: function (eventInfo) {
 
             var fid = this.id;
-            
             var client = new Windows.Web.Http.HttpClient();
-
             var uri = "http://fleetmgr2212.appspot.com/user/" + WinJS.Application.sessionState.uid.toString() + "/specCar/" + WinJS.Application.sessionState.sid.toString() + "/fillUp/" + fid.toString();
 
             client.deleteAsync(new Windows.Foundation.Uri(uri)).done(function (result) {
                 document.getElementById("output").innerHTML = "Fill Up deleted.";
                 setTimeout(function () { WinJS.Navigation.navigate('/pages/speccar/speccar.html'); }, 1000);
-                
             });
         },
 
        addClickHandler: function (eventInfo) {
           
            document.getElementById("error").innerHTML = "";
-
            getPosDone = false;
            errHandlerDone = false;
-
            var err_msg;
            
-
            var date = document.getElementById("dateInput").value;
            var distance = document.getElementById("distanceInput").value;
            var fuelused = document.getElementById("fuelusedInput").value;
@@ -138,10 +116,8 @@
            var validFuelused = floatPattern.test(fuelused);
 
            if (!validDate) {
-
                //print error message
                document.getElementById("error").innerHTML += "Error: Date must be formatted as MM-DD-YYYY<br>";
-
            }
 
            if (!validDistance) {
@@ -157,20 +133,16 @@
                }
            }
            
-           
             // Add new fillup to API
-           
            if (validDate && validDistance && validFuelused) {
+               
                var client = new Windows.Web.Http.HttpClient();
                var payload = "date=" + date + "&distance=" + distance + "&fuelused=" + fuelused;
 
                if (lat != null && long != null) {
-
                    payload += "&latitude=" + lat + "&longitude=" + long;
-                   
                }
 
-               //var uri = "http://fleetmgr2212.appspot.com/user/5707702298738688/specCar/" + WinJS.Application.sessionState.sid.toString() + "/fillUp";
                var uri = "http://fleetmgr2212.appspot.com/user/" + WinJS.Application.sessionState.uid.toString() + "/specCar/" + WinJS.Application.sessionState.sid.toString() + "/fillUp";
 
                client.postAsync(new Windows.Foundation.Uri(uri), Windows.Web.Http.HttpStringContent(payload, Windows.Storage.Streams.UnicodeEncoding.utf8, 'application/x-www-form-urlencoded')).done(function (result) {
@@ -180,7 +152,6 @@
                    client.getAsync(new Windows.Foundation.Uri(uri)).done(function (result) {
 
                        var jsonResult = JSON.parse(result.content.toString());
-
                        document.getElementById("fillupList").innerHTML = "";
 
                        for (var i in jsonResult.fillUps) {
@@ -193,50 +164,39 @@
                            }
 
                            out += '<br><button class="delete" id="' + jsonResult.fillUps[i].key.toString() + '">Delete</button><br><br>';
-                           //out += '<br><br>';
                            document.getElementById("fillupList").innerHTML += out;
 
                        }
                        
                        WinJS.Utilities.query("button.delete").listen("click", function (eventInfo) {
+                           
                            var fid = this.id;
-
                            var client = new Windows.Web.Http.HttpClient();
-
                            var uri = "http://fleetmgr2212.appspot.com/user/" + WinJS.Application.sessionState.uid.toString() + "/specCar/" + WinJS.Application.sessionState.sid.toString() + "/fillUp/" + fid.toString();
 
                            client.deleteAsync(new Windows.Foundation.Uri(uri)).done(function (result) {
                                document.getElementById("output").innerHTML = "Fill Up deleted.";
                                setTimeout(function () { WinJS.Navigation.navigate('/pages/speccar/speccar.html'); }, 1000);
-
                            });
+                           
                        }, false);
 
                    });
                });
-              
-             
            }
-            
-           
-
-            
        },
        
-
-        unload: function () {
+       unload: function () {
             // TODO: Respond to navigations away from this page.
         },
 
         updateLayout: function (element) {
             /// <param name="element" domElement="true" />
-
             // TODO: Respond to changes in layout.
         }
     });
 
     // geolocation code
-
     function getLoc () {
         if (loc == null) {
             loc = new Windows.Devices.Geolocation.Geolocator();
@@ -248,27 +208,22 @@
     }
 
     function getPositionHandler (pos) {
+        
         lat = pos.coordinate.point.position.latitude;
         long = pos.coordinate.point.position.longitude;
         
         if (document.getElementById("loc_status")) {
             document.getElementById('loc_status').innerHTML = 'Location found:<br>lat = ' + lat.toString() + '<br>long= ' + long.toString();
         }
-        
-        
+
         getPosDone = true;
-        //document.getElementById('accuracy').innerHTML = pos.coordinate.accuracy;
-        //document.getElementById('geolocatorStatus').innerHTML =
-        //  getStatusString(loc.locationStatus);
+       
     }
 
     function errorHandler (e) {        
         loc_err = e.message;
         document.getElementById('loc_status').innerHTML = "ERROR: could not find location";
         errHandlerDone = true;
-        // Display an appropriate error message based on the location status.
-        //document.getElementById('geolocatorStatus').innerHTML =
-        //   getStatusString(loc.locationStatus);    
     }
 
 })();
